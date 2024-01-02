951efbe1befca8efefd4b83f156d751ce8847826
cjj <jijian2077@gmail.com>
2024-01-02 11:03:24 +0800

    新增member模块

--- memeber/src/main/java/com/cjj/train/member/MemberApplication.java
+++ memeber/src/main/java/com/cjj/train/member/MemberApplication.java
@@ -1,13 +1,13 @@
-package com.cjj.train;
+package com.cjj.train.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrainApplication {
+public class MemberApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TrainApplication.class, args);
+        SpringApplication.run(MemberApplication.class, args);
     }
 
 }
