0cdd4bbeff90c5b403214f5f2d05d4fbd23ae2d4
cjj <jijian2077@gmail.com>
2024-01-02 21:57:53 +0800

    封装请求参数和返回结果

--- memeber/src/main/java/com/cjj/train/member/controller/MemberController.java
+++ memeber/src/main/java/com/cjj/train/member/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.cjj.train.member.controller;
 
+import com.cjj.train.common.resp.CommonResp;
+import com.cjj.train.member.req.MemberRegisterReq;
 import com.cjj.train.member.service.MemberService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +17,20 @@ public class MemberController {
     @Autowired
     public MemberService memberService;
     @GetMapping("/count")
-    public int count() {
-        return memberService.count();
+    public CommonResp<Integer> count() {
+        CommonResp<Integer> commonResp = new CommonResp<>();
+        int count = memberService.count();
+        commonResp.setContent(count);
+        return commonResp;
+    }
+
+    //注册用户
+    @PostMapping("/register")
+    public CommonResp<Long> register(MemberRegisterReq memberRegisterReq) {
+//        long mobile = memberService.register(memberRegisterReq);
+//        CommonResp<Long> commonResp = new CommonResp<>();
+//        commonResp.setContent(mobile);
+//        return commonResp;
+        return new CommonResp<Long>(memberService.register(memberRegisterReq));
     }
 }
--- memeber/src/main/java/com/cjj/train/member/service/MemberService.java
+++ memeber/src/main/java/com/cjj/train/member/service/MemberService.java
@@ -1,8 +1,15 @@
 package com.cjj.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.cjj.train.member.domain.Member;
+import com.cjj.train.member.domain.MemberExample;
 import com.cjj.train.member.mapper.MemberMapper;
+import com.cjj.train.member.req.MemberRegisterReq;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +19,22 @@ public class MemberService {
 
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(MemberRegisterReq memberRegisterReq) {
+        String mobile = memberRegisterReq.getMobile();
+        //select * from member where mobile = xxx;
+        //构造查询条件，号码不能重复
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        if(CollUtil.isNotEmpty(members)) {
+            throw new RuntimeException("电话号码已经存在");
+        }
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
