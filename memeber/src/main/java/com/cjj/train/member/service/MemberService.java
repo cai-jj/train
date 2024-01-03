475fcb7cc8129b665a82ad1951a04a504673b84a
cjj <jijian2077@gmail.com>
2024-01-03 11:59:10 +0800

    使用雪花算法生成全局唯一自增id

--- memeber/src/main/java/com/cjj/train/member/service/MemberService.java
+++ memeber/src/main/java/com/cjj/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.cjj.train.member.service;
 import cn.hutool.core.collection.CollUtil;
 import com.cjj.train.common.exception.BusinessException;
 import com.cjj.train.common.exception.BusinessExceptionEnum;
+import com.cjj.train.common.util.SnowUtil;
 import com.cjj.train.member.domain.Member;
 import com.cjj.train.member.domain.MemberExample;
 import com.cjj.train.member.mapper.MemberMapper;
@@ -34,7 +35,7 @@ public class MemberService {
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
         Member member = new Member();
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
