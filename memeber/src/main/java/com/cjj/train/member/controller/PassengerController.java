df0d37365ac6e397c90010ff0adfa11ea42e1a14
cjj <jijian2077@gmail.com>
2024-01-05 16:29:51 +0800

    新增乘客查询接口

--- memeber/src/main/java/com/cjj/train/member/controller/PassengerController.java
+++ memeber/src/main/java/com/cjj/train/member/controller/PassengerController.java
@@ -1,14 +1,16 @@
 package com.cjj.train.member.controller;
 
+import com.cjj.train.common.context.LoginMemberContext;
 import com.cjj.train.common.resp.CommonResp;
+import com.cjj.train.member.req.PassengerQueryReq;
 import com.cjj.train.member.req.PassengerSaveReq;
+import com.cjj.train.member.resp.PassengerQueryResp;
 import com.cjj.train.member.service.PassengerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -21,5 +23,12 @@ public class PassengerController {
     public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq passengerSaveReq) {
         passengerService.save(passengerSaveReq);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq passengerQueryReq) {
+        passengerQueryReq.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> list = passengerService.queryList(passengerQueryReq);
+        return new CommonResp<>(list);
     }
 }
--- memeber/src/main/java/com/cjj/train/member/service/PassengerService.java
+++ memeber/src/main/java/com/cjj/train/member/service/PassengerService.java
@@ -6,16 +6,22 @@ import cn.hutool.core.util.ObjectUtil;
 import com.cjj.train.common.context.LoginMemberContext;
 import com.cjj.train.common.util.SnowUtil;
 import com.cjj.train.member.domain.Passenger;
+import com.cjj.train.member.domain.PassengerExample;
 import com.cjj.train.member.mapper.PassengerMapper;
+import com.cjj.train.member.req.PassengerQueryReq;
 import com.cjj.train.member.req.PassengerSaveReq;
+import com.cjj.train.member.resp.PassengerQueryResp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
 
     @Autowired
     private PassengerMapper passengerMapper;
+
     public void save(PassengerSaveReq passengerSaveReq) {
         Passenger passenger = BeanUtil.copyProperties(passengerSaveReq, Passenger.class);
         DateTime now = DateTime.now();
@@ -25,5 +31,17 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+
+    public List<PassengerQueryResp> queryList(PassengerQueryReq passengerQueryReq) {
+        Long memberId = passengerQueryReq.getMemberId();
+        PassengerExample passengerExample = new PassengerExample();
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        if(ObjectUtil.isNotNull(memberId)) {
+            criteria.andMemberIdEqualTo(memberId);
+        }
+        List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+        List<PassengerQueryResp> list = BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
+        return list;
     }
 }
