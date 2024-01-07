3aff450be6c46cb9d7c48eb3e344fef4ad21f4d5
cjj <jijian2077@gmail.com>
2024-01-07 15:39:16 +0800

    添加车次功能

---
+++ business/src/main/java/com/cjj/train/business/req/TrainQueryReq.java
@@ -0,0 +1,13 @@
+package com.cjj.train.business.req;
+
+
+import com.cjj.train.common.req.PageReq;
+
+public class TrainQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainQueryReq{" +
+                "} " + super.toString();
+    }
+}
