e121745ed4e2ef44b87b39474b16a6f04994889f
cjj <jijian2077@gmail.com>
2024-01-07 15:05:02 +0800

    添加站台功能

---
+++ business/src/main/java/com/cjj/train/business/req/StationQueryReq.java
@@ -0,0 +1,13 @@
+package com.cjj.train.business.req;
+
+
+import com.cjj.train.common.req.PageReq;
+
+public class StationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "StationQueryReq{" +
+                "} " + super.toString();
+    }
+}
--- common/src/main/java/com/cjj/train/common/exception/BusinessExceptionEnum.java
+++ common/src/main/java/com/cjj/train/common/exception/BusinessExceptionEnum.java
@@ -1,12 +1,18 @@
 package com.cjj.train.common.exception;
 
 public enum BusinessExceptionEnum {
+
     MEMBER_MOBILE_EXIST("手机号已注册"),
     MEMBER_MOBILE_NOT_EXIST("请先获取短信验证码"),
-    MEMBER_MOBILE_CODE_ERROR("验证码不正确，请重新输入");
+    MEMBER_MOBILE_CODE_ERROR("短信验证码错误"),
+
+    BUSINESS_STATION_NAME_UNIQUE_ERROR("车站已存在"),
+    BUSINESS_TRAIN_CODE_UNIQUE_ERROR("车次编号已存在"),
+    BUSINESS_TRAIN_STATION_INDEX_UNIQUE_ERROR("同车次站序已存在"),
+    BUSINESS_TRAIN_STATION_NAME_UNIQUE_ERROR("同车次站名已存在"),
+    BUSINESS_TRAIN_CARRIAGE_INDEX_UNIQUE_ERROR("同车次厢号已存在");
 
     private String desc;
-
 
     BusinessExceptionEnum(String desc) {
         this.desc = desc;
@@ -24,6 +30,6 @@ public enum BusinessExceptionEnum {
     public String toString() {
         return "BusinessExceptionEnum{" +
                 "desc='" + desc + '\'' +
-                '}';
+                "} " + super.toString();
     }
 }
