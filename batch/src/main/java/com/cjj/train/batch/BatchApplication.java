bca794a9369e1742f84f3dd35ba5348b72f39f86
cjj <jijian2077@gmail.com>
2024-01-08 14:57:19 +0800

    为batch模块配置dao层

--- batch/src/main/java/com/cjj/train/batch/BatchApplication.java
+++ batch/src/main/java/com/cjj/train/batch/BatchApplication.java
@@ -1,4 +1,4 @@
-package com.cjj.tain.batch;
+package com.cjj.train.batch;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
--- batch/src/main/java/com/cjj/train/batch/controller/TestController.java
+++ batch/src/main/java/com/cjj/train/batch/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.cjj.tain.batch.controller;
+package com.cjj.train.batch.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
