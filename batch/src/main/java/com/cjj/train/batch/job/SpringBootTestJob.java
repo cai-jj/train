62cc994ceabaae4b2896b5bf1866b670cd94b201
cjj <jijian2077@gmail.com>
2024-01-08 16:12:21 +0800

    quartz功能完善，前端页面可以手动添加定时任务

--- batch/src/main/java/com/cjj/train/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/cjj/train/batch/job/SpringBootTestJob.java
@@ -4,12 +4,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
-@EnableScheduling
-public class SpringBootTestJob {
-
-    @Scheduled(cron = "0/5 * * * * ?")
-    public void test() {
-        System.out.println("SpringBootJob test");
-    }
-}
+//@Component
+//@EnableScheduling
+//public class SpringBootTestJob {
+//
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    public void test() {
+//        System.out.println("SpringBootJob test");
+//    }
+//}
--- batch/src/main/java/com/cjj/train/batch/job/TestJob.java
+++ batch/src/main/java/com/cjj/train/batch/job/TestJob.java
@@ -1,12 +1,20 @@
 package com.cjj.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-
+@DisallowConcurrentExecution//禁止并发执行
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("test job");
+        System.out.println("开始 job");
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println("结束 job");
+
     }
 }
