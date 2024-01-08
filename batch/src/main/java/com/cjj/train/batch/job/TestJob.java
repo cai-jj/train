3a6147ec84ea2bb7c430bd47f7845cb762330f01
cjj <jijian2077@gmail.com>
2024-01-08 15:16:45 +0800

    集成quartz

---
+++ batch/src/main/java/com/cjj/train/batch/job/TestJob.java
@@ -0,0 +1,12 @@
+package com.cjj.train.batch.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TestJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("test job");
+    }
+}
